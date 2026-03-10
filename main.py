83f43e1211d9820607ef7cb5ba44cd052e554537
BodyaX <abaharev340@gmail.com>
2026-03-10 22:53:40 +0500

    init dispatcher and small to aiogram

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ import os
 import asyncio
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher, types
-from aiogramm.filters import Command
+from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
 
 
 
-load_dotenv
+load_dotenv() #загрузить
+bot = Bot(token=os.getenv("BOT_TOKEN")) #токен
+dp = Dispatcher() #мозг
+
+
