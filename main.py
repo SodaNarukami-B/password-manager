d076b9436f51a4a1327a3e9f4d30e9ca3599e169
BodyaX <abaharev340@gmail.com>
2026-03-10 22:49:22 +0500

    import all libs

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+import os
+import asyncio
+from dotenv import load_dotenv
+from aiogram import Bot, Dispatcher, types
+from aiogramm.filters import Command
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, State
 
+
+
+load_dotenv
